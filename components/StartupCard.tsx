5119cc2dde5b13c2a2ed081eea60084f6fe75abf
blackcat13 <detective.blackcat13@gmail.com>
2024-11-10 17:59:42 +0700

    fetch data and type safety

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,25 +4,26 @@ import {EyeIcon} from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import {Author, Startup} from "@/sanity/types";
+
+export type StartupCardType = Omit<Startup, "author"> & { author?: Author };
 
 const StartupCard = ({post}: { post: StartupCardType }) => {
   const {
     _createdAt,
     views,
-    author: {_id: authorId, name: authorName},
+    author,
     title,
     category,
     _id,
     image,
-    desciption
+    description
   } = post;
 
   return (
     <li className={"startup-card group"}>
       <div className={"flex-between"}>
-        <p className={"startup-card_date"}>
-          {formatDate(_createdAt)}
-        </p>
+        <p className={"startup-card_date"}> {formatDate(_createdAt)} </p>
 
         <div className={"flex gap-1.5"}>
           <EyeIcon className={"size-6 text-primary"}/>
@@ -32,16 +33,16 @@ const StartupCard = ({post}: { post: StartupCardType }) => {
 
       <div className={"flex-between mt-5 gap-5"}>
         <div className={"flex-1"}>
-          <Link href={`/user/${authorId}`}>
+          <Link href={`/user/${author?._id}`}>
             <p className={"text-16-medium line-clamp-1"}>
-              {authorName}
+              {author?.name}
             </p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className={"text-26-semibold line-clamp-1"}>{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image
             src={"https://placehold.co/48x48"}
             alt={"placeholder"}
@@ -51,13 +52,13 @@ const StartupCard = ({post}: { post: StartupCardType }) => {
       </div>
       <Link href={`/startup/${_id}`}>
         <p className={"startup-card_desc"}>
-          {desciption}
+          {description}
         </p>
         <img src={image} alt="placeholder" className={"startup-card_img"}/>
       </Link>
 
       <div className={"flex-between gap-3 mt-5"}>
-        <Link href={`/?query=${category.toLowerCase()}`}>
+        <Link href={`/?query=${category?.toLowerCase()}`}>
           <p className={"text-16-medium"}>{category}</p>
         </Link>
         <Button className={"startup-cart_btn"} asChild>
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -6,5 +6,5 @@ export const structure: StructureResolver = (S) =>
     .title('Content')
     .items([
       S.documentTypeListItem("author").title("Authors"),
-      S.documentTypeListItem("startup").title("Starups"),
+      S.documentTypeListItem("startup").title("Startups"),
     ]);
