71cce87fcdf37dd595ad1d9ccbf078b7d0ae5750
blackcat13 <detective.blackcat13@gmail.com>
2024-11-18 00:25:50 +0700

    update next config

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -29,7 +29,8 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border bg-background text-foreground text-slate-950" +
+          " dark:bg-slate-950 dark:text-slate-50",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
